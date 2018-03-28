8d16a2b1bb8ec67a36decc079f5b88f42d27756e
Jasper Lorenzen <jasperlorenzen@web.de>
2018-03-28 17:36:20 +0200

    Der User bekommt nun nurnoch die Categorien und Kunden, die er auch sehen kann

--- ajax/orders_read_productCategories.php
+++ ajax/orders_read_productCategories.php
@@ -1,8 +1,20 @@
 <?php
+session_start();
 include('../admin/db_crud.php');
 
 $db = new db_connection();
-$data = $db->getData("productCategories", array('id','name'));
+
+$userId = $_SESSION['userid'];
+
+$visibleCategories = $db->getData("categoryrelations", "idProductCat", "idUserCat = '".$userId."'");
+
+$whereCondition = "";
+foreach($visibleCategories as $category){
+    $whereCondition .= $category['idProductCat'].' or ';
+}
+$whereCondition = rtrim($whereCondition, " or ");
+
+$data = $db->getData("productCategories", array('id','name'), "id = ".$whereCondition);
 
 
 $jsonData = json_encode($data);
--- ajax/orders_read_products.php
+++ ajax/orders_read_products.php
@@ -1,8 +1,22 @@
 <?php
+session_start();
 include('../admin/db_crud.php');
 
 $db = new db_connection();
-$data = $db->getData("products", array('id','productID','name','productCategory'));
+
+
+
+$userId = $_SESSION['userid'];
+
+$visibleCategories = $db->getData("categoryrelations", "idProductCat", "idUserCat = '".$userId."'");
+
+$whereCondition = "";
+foreach($visibleCategories as $category){
+    $whereCondition .= $category['idProductCat'].' or ';
+}
+$whereCondition = rtrim($whereCondition, " or ");
+
+$data = $db->getData("products", array('id','productID','name','productCategory'), "productCategory = ".$whereCondition);
 
 
 $jsonData = json_encode($data);
